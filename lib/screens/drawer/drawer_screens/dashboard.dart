2248fa9d92a12ef79de079446882b1d79886c6d1
rsamirdevone <artemfokinorgin@gmail.com>
2023-03-01 17:53:01 +0500

    problem to link icon

--- lib/screens/drawer/drawer_screens/dashboard.dart
+++ lib/screens/drawer/drawer_screens/dashboard.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
 
@@ -12,9 +10,131 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Dashboard'),
+    return Scaffold(
+      backgroundColor: Color.fromARGB(224, 224, 224, 224),
+      body: Container(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Padding(padding: EdgeInsets.only(top: 100, left: 10)),
+                Container(
+                  width: 110,
+                  height: 75,
+                  decoration: BoxDecoration(
+                    color: Colors.blue[700],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Center(
+                    child: Text(
+                      '+500',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 11,
+                ),
+                Container(
+                  width: 120,
+                  height: 75,
+                  decoration: BoxDecoration(
+                    color: Colors.pink[700],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Center(
+                    child: Text(
+                      '+300',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 11,
+                ),
+                Container(
+                  width: 120,
+                  height: 75,
+                  decoration: BoxDecoration(
+                    color: Colors.green[500],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Center(
+                    child: Text(
+                      '+1200',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Container(
+                  width: 370,
+                  height: 260,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Sales',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Column(
+              children: [
+                Container(
+                  width: 370,
+                  height: 260,
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(12)),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Activites',
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
